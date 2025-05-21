a4a80022a6d176dc0b36aedbb98cc63200107bc1
Cristhian Sanchez <crivisan1994@gmail.com>
2025-05-21 16:02:30 +0200

    add measurement tool, segments and polygons

--- web-mapping-application/scripts/layers.js
+++ web-mapping-application/scripts/layers.js
@@ -50,7 +50,7 @@ dragElement(document.getElementById("draggable"));
 
 function dragElement(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById(elmnt.id + "header")) {
+    if (document.getElementById(elmnt.id + "-header")) {
         // if present, the header is where you move the DIV from:
         document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
     } else {
